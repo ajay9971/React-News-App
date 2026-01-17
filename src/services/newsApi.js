521a7d7d140322f64f09f11fc6d536a826373a00
ajay9971e <ajaykumar997140@gmail.com>
2026-01-17 20:25:34 +0530

    default

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,8 +1,8 @@
-const VITE_API_KEY = "c9d250d877c6bee5f1d5d98f370a3c21";
+const API_KEY = "c9d250d877c6bee5f1d5d98f370a3c21";
 const BASE_URL="https://gnews.io/api/v4/top-headlines"
 
 
-export const fetchNews=async(query="",category="", page=1
+export const fetchNews=async(query="",category="", page=1)=>{
     let url = `${BASE_URL}?lang=en&country=in&token=${API_KEY}&page=${page}&max=10`;
     if (query)url +=`&q=${query}`;
     if(category)url +=`&topic=${category}`;
